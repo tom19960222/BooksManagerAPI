86ec67b0d7140b8bc05014659ccbc2b1eac23961
爆頭專家 <tom19960222@gmail.com>
2015-05-05 21:48:47 +0800

    token api finished (use mongodb)

--- main.py
+++ main.py
@@ -3,52 +3,14 @@ from flask import Flask
 from booksapi import booksapi
 from tagsapi import tagsapi
 from usersapi import usersapi
+from tokenapi import tokenapi
+from pymongo import MongoClient
 
 app = Flask(__name__)
 app.register_blueprint(booksapi)
 app.register_blueprint(tagsapi)
 app.register_blueprint(usersapi)
-
-"""
-books = [
-    {
-	'book_id' : 0,
-	'bookname' : '雨港基隆 - 桐花雨',
-	'author' : '東方紅',
-	'publisher' : '尖端出版',
-	'publish_date' : '20150101',
-	'price' : 200,
-	'ISBN' : '1234567890'
-    }
-]
-"""
-users = [
-    {
-	'user_id' : 0,
-	'username' : 'Ananymous',
-	'email' : '',
-	'password' : '',
-	'lastlogintime' : '20150408 15:59',
-	'lastloginip' : '127.0.0.1'
-    }
-]
-
-access_tokens = [
-    {
-	'token' : 'fjrubrifgkdjnidjvisdjoj',
-	'user_id' : 0,
-	'expire_time' : '20150409 15:59'
-    } 
-]
-
-tags = [
-    {
-	'ISBN' : '1234567890',
-	'tags' : '輕小說',
-	'user_id' : 0
-    }
-]
-
+app.register_blueprint(tokenapi)
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
---
+++ tokenapi.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+# coding: UTF-8
+from flask import Blueprint, abort
+from bson.json_util import dumps
+from pymongo import MongoClient
+import string, random, time
+
+tokenapi = Blueprint('tokenapi', __name__, url_prefix='/api/token')
+dbClient = MongoClient('163.13.128.116', 27017)
+db = dbClient.BooksManagerTest1
+tokensdb = db.tokens
+
+access_tokens = [
+    {
+	'token' : 'fjrubrifgkdjnidjvisdjoj',
+	'user_id' : 0,
+	'expire_time' : 1429867579
+    }
+]
+
+expire_seconds = 86400
+
+def generateAccessToken():
+    return ''.join(random.choice(string.ascii_lowercase + string.digits) for _ in range(32))
+
+@tokenapi.route('', methods=["GET"])
+def get_token():
+
+    tmptoken = {
+        'token': generateAccessToken(),
+        'user_id': 0,
+        'expire_time': int(time.time()) + expire_seconds
+    }
+    tokensdb.insert_one(tmptoken)
+    access_tokens.append(tmptoken)
+    return dumps(tmptoken)
+
+@tokenapi.route('/<token>', methods=["GET"])
+def get_token_by_token(token):
+    """tmptoken = [tmptoken for tmptoken in access_tokens if tmptoken['token'] == token]"""
+    if token == "ALL":
+        tmptoken = tokensdb.find()
+    else:
+        tmptoken = tokensdb.find_one({'token': token})
+        if len(tmptoken) == 0:
+            abort(404)
+    return dumps(tmptoken)
