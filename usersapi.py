1fe1fde218b2d39ab42ae523943cc38ebd986bfb
爆頭專家 <tom19960222@gmail.com>
2015-05-05 23:08:49 +0800

    userapi GET&POST using mongodb now
    also changed user’s data structure

--- usersapi.py
+++ usersapi.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 # coding: UTF-8
 from flask import Blueprint, jsonify, abort, request
+from pymongo import MongoClient
+from bson.json_util import dumps
 
 usersapi = Blueprint('userapi', __name__, url_prefix='/api/user')
+dbClient = MongoClient('163.13.128.116', 27017)
+db = dbClient.BooksManagerTest1
+usersdb = db.users
 
 users = [
     {
@@ -11,22 +16,23 @@ users = [
         'email': '',
         'password': '',
         'lastlogintime': '2015/04/08 15:59',
-        'lastloginip': '127.0.0.1'
+        'lastloginip': '127.0.0.1',
+        'deactivated': True
     }
 ]
 
 @usersapi.route('', methods=['GET'])
 def list_all_users():
-    return jsonify({'users': users})
+    return dumps(usersdb.find({'deactivated': False}))
 
 
 @usersapi.route('/<int:user_id>', methods=['GET'])
 def get_user_by_id(user_id):
-    tmpusers = [tmpuser for tmpuser in users if tmpuser['user_id'] == user_id]
-
+    #tmpusers = [tmpuser for tmpuser in users if tmpuser['user_id'] == user_id]
+    tmpusers = usersdb.find_one({'user_id': user_id})
     if len(tmpusers) == 0:
         abort(404)
-    return jsonify({'users': tmpusers[0]})
+    return dumps(tmpusers)
 
 
 @usersapi.route('', methods=['POST'])
@@ -34,14 +40,23 @@ def add_user():
     jsondata = request.get_json()
     if not jsondata or not 'username' in jsondata:
         abort(400)
+
+    tmpusers = usersdb.find().sort([('user_id', -1)]).limit(1)
+    for user in tmpusers:
+        lastuser = user
+    new_user_id = 1
+    if lastuser['user_id'] is not None:
+        new_user_id = int(lastuser['user_id'])+1
+
     tmpuser = {
-        'user_id': users[-1]['user_id'] + 1,
+        'user_id': new_user_id,
         'username': jsondata['username'],
         'email': jsondata['email'],
-        'password': jsondata['password']
+        'password': jsondata['password'],
+        'deactivated': False
     }
-    users.append(tmpuser)
-    return jsonify({'user': tmpuser}), 201
+    usersdb.insert(tmpuser)
+    return dumps(tmpuser), 201
 
 
 @usersapi.route('/<int:user_id>', methods=['DELETE'])
@@ -69,3 +84,4 @@ def update_user(user_id):
     if 'password' in request.json and type(request.json['password']) is unicode:
         tmpuser[0]['password'] = jsondata['password']
         return jsonify({'book': tmpuser[0]})
+
