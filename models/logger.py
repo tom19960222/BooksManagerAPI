729a052cdcabc89c70d48568bbf3745402c704a2
爆頭專家 <tom19960222@gmail.com>
2015-05-25 23:15:46 +0800

    Logger log to file now.

--- models/logger.py
+++ models/logger.py
@@ -1,4 +1,7 @@
 import time, datetime
 def log(msg):
     time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    logfilename = datetime.datetime.now().strftime("%Y-%m-%d")
+    logfile = open("%s.log" % (logfilename), 'a')
+    logfile.write("%s: %s\n" % (time, msg))
     print("%s: %s" % (time, msg))
