957615ce53890cd66203b9975f3bb20b7a878c74
爆頭專家 <tom19960222@gmail.com>
2015-05-27 00:14:29 +0800

    Edit login response from only message to the dump of user data.

--- models/users.py
+++ models/users.py
@@ -107,7 +107,7 @@ def login(email, password, token):
     if tmpuser['password'] == password:
         changeTokenUser(token, tmpuser['user_id'])
         log("User %s logged in with token %s" % (email, token))
-        return JSONResponseLoginSuccessful
+        return JSONResponse(dumps(tmpuser))
     else:
         log(("User %s logged in with wrong password") % email)
         return JSONResponseWrongPassword
