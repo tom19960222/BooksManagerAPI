1f426428992c166f912ecb21dd0614fdbd24efb7
爆頭專家 <tom19960222@gmail.com>
2015-05-29 00:26:21 +0800

    Add a new path to upload user head image and edit user's head_image_url at the same time.

--- controllers/fileuploadapi.py
+++ controllers/fileuploadapi.py
@@ -4,6 +4,7 @@ from views.JSONResponse.CommonJSONResponse import *
 from views.templates.JSONResponse import makeResponse
 from models.fileupload import save_upload_file
 from models.utils.userutils import get_user_id_by_token
+from models.users import update_user
 
 fileuploadapi = Blueprint('fileuploadapi', __name__, url_prefix='/api/upload')
 BASE_URL = 'http://163.13.128.116:5001/'
@@ -17,8 +18,22 @@ def upload_file():
     user_id = get_user_id_by_token(token)
     if user_id == 0:
         return makeResponse(JSONResponseLoginFirst)
-    print("%s: %s" % (token, user_id))
     f = request.files['cover_image']
     save_upload_file(f, user_id)
 
     return jsonify({'cover_image_url': "%s%s/%s" % (BASE_URL, user_id, secure_filename(f.filename))})
+
+@fileuploadapi.route('/user_head_image', methods=['POST'])
+def upload_user_head():
+    token = request.headers.get('Token')
+    if token is None:
+        return makeResponse(JSONResponseProvideToken)
+
+    user_id = get_user_id_by_token(token)
+    if user_id == 0:
+        return makeResponse(JSONResponseLoginFirst)
+    f = request.files['head_image']
+    save_upload_file(f, user_id)
+    image_url = "%s%s/%s" % (BASE_URL, user_id, secure_filename(f.filename))
+    update_user(user_id, head_image_url=image_url)
+    return jsonify({'head_image_url': image_url})
